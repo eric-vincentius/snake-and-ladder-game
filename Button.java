fc9cd9454e6662ff0221ed54c65f1d9b408fb1cd
eric-vincentius <144997327+eric-vincentius@users.noreply.github.com>
2024-06-28 14:32:54 +0700

    Added group identity

--- Button.java
+++ Button.java
@@ -1,3 +1,18 @@
+/**
+ * -----------------------------------------------------
+ * ES234211 - Programming Fundamental
+ * Genap - 2023/2024
+ * Group Capstone Project: Snake and Ladder Game
+ * -----------------------------------------------------
+ * Class    : C
+ * Group    : 12
+ * Members  :
+ * 1. 5026231103 - Eric Vincentius Jaolis
+ * 2. 5026231085 - Firmansyah Adi Prasetyo
+ * 3. 5026231206 - Rafael Dimas Khristianto
+ * ------------------------------------------------------
+ */
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
